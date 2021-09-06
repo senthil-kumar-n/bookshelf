c0708e3585ad6ba7dadf1da5e9734c462e09e6f6
senthil-kumar-n <chinnapayan@gmail.com>
2021-09-06 12:23:34 +0530

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from sqlite3.dbapi2 import connect
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
@@ -7,15 +8,31 @@ app = Flask(__name__)
 def hello_world():
     return render_template("home.html")
 
-@app.route("/list")
+@app.route("/list", methods = ['POST', 'GET'])
 def bklist():
-    conn = sqlite3.connect("dbfile/bookbase.db")
-    conn.row_factory = sqlite3.Row
-    cur=conn.cursor()
-    cur.execute("SELECT * FROM booklist")
-    rows = cur.fetchall()
-    conn.close()
-    return render_template("list.html", rows = rows)
+    if request.method == 'GET':
+        conn = sqlite3.connect("dbfile/bookbase.db")
+        conn.row_factory = sqlite3.Row
+        cur=conn.cursor()
+        cur.execute("SELECT * FROM booklist")
+        rows = cur.fetchall()
+        conn.close()
+        return render_template("list.html", rows = rows)
+    elif request.method == 'POST':
+        searchvar = request.form['bsearch']
+        searchvar1 = request.form['bauthor']          
+        conn = sqlite3.connect("dbfile/bookbase.db")    
+        conn.row_factory = sqlite3.Row
+        cur=conn.cursor()    
+        cur.execute("SELECT * FROM booklist WHERE bookname like ? or author like ? ",('%'+searchvar+'%','%'+searchvar1+'%',))
+        #cur.execute("SELECT * FROM booklist WHERE author like ? ",('%'+searchvar1+'%',))
+        rows = cur.fetchall()
+        conn.close()
+        print (searchvar)
+        print (searchvar1)
+        print(rows)                      
+        return render_template("list.html", rows = rows)
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
--- dbconnection.py
+++ dbconnection.py
@@ -6,7 +6,18 @@ def view():
     cur.execute("SELECT * FROM booklist")
     rows = cur.fetchall()
     conn.close()
-    print (rows)
-    print(type(rows))
+    #return render_template("list.html", rows = rows)
+    return rows
+    
 
-print(view())
+def searchfun(bauthor=""):
+    conn = sqlite3.connect("dbfile/bookbase.db")
+    cur=conn.cursor()    
+    cur.execute("SELECT * FROM booklist WHERE author OR bookname like ?" ,('%'+bauthor+'%',))
+    #  "select string from stringtable where string like ? and type = ?", ('%'+searchstr+'%', type))
+    #cur.execute("SELECT * FROM booklist WHERE bookname like ? OR author like ? ",('%'+searchvar+'%','%'+searchvar1+'%'))
+    rows = cur.fetchall()
+    conn.close()
+    return rows    
+print(searchfun("walt"))
+#print(view()
